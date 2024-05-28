2b033df48ea423cf86982f726cb9f826d98c317a
MatundingCalculator <zianwayne14@gmail.com>
2024-05-28 16:35:05 +0800

    Initial commit

---
+++ app/src/main/java/com/example/abendaniowagecalculator/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.abendaniowagecalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText num1;
+    EditText num2;
+    TextView result;
+    Button calculate;
+    Button clear;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        num1 = findViewById(R.id.txtNum1);
+        num2 = findViewById(R.id.txtNum2);
+        result = findViewById(R.id.txtResult);
+        calculate = findViewById(R.id.btnCalculate);
+        clear = findViewById(R.id.btnClear);
+
+        calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double number1 = Double.parseDouble(num1.getText().toString());
+                double number2 = Double.parseDouble(num2.getText().toString());
+                double product = number1 * number2;
+                result.setText(String.valueOf(product));
+
+            }
+        });
+
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                num1.setText(" ");
+                num2.setText(" ");
+                result.setText("00.00");
+            }
+        });
+
+
+
+    }
+}
